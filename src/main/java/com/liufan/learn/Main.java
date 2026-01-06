8e57b344b4bb6c1247d277e44f089ef2036ca1e6
刘帆 <evl4219513@foxmail.com>
2026-01-06 19:07:19 +0800

    add 变量

--- src/main/java/com/liufan/learn/Main.java
+++ src/main/java/com/liufan/learn/Main.java
@@ -1,11 +1,19 @@
 package com.liufan.learn;
 
+/**
+ * <ol>
+ *     <li>一个 Java 源文件可以包含多个类的定义，但只能定义一个 public 类，且 public 类名必须与文件名一致；</li>
+ *     <li>没有明确写 extends 的类，编译器会自动加上 extends Object。</li>
+ * </ol>
+ */
 public class Main {
 
 	/**
-     * Java规定，某个类定义的public static void main(String[] args)是Java程序的固定入口方法，Java程序总是从main方法开始执行。
+     * Java 规定，某个类定义的 public static void main(String[] args) 是 Java 程序的固定入口方法，Java 程序总是从 main 方法开始执行。
      */
 	public static void main(String[] args) {
-		System.out.println("Hello, world! 你好，世界！");
+        System.out.println("Hello, world! 你好，世界！");
+        // LearnVar.practice(); // 1、变量
+        LearnBasicDataType.practice(); // 2、基本数据类型
 	}
 }
